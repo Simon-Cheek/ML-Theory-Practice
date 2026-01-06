fdf31fcbe68888e51dfa616055407412fa11a7f2
Simon-Cheek <simon.cheek@yahoo.com>
2026-01-06 09:38:31 -0500

    Add decision tree

---
+++ DecisionTrees/DecisionTrees.py
@@ -0,0 +1,18 @@
+from sklearn.datasets import load_iris
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+
+iris = load_iris(as_frame=True)
+X_iris = iris.data[["petal length (cm)", "petal width (cm)"]].values
+y_iris = iris.target
+
+tree_clf = DecisionTreeClassifier(max_depth=2, random_state=42)
+tree_clf.fit(X_iris, y_iris)
+
+export_graphviz(
+        tree_clf,
+        out_file="iris_tree.dot",
+        feature_names=["petal length (cm)", "petal width (cm)"],
+        class_names=iris.target_names,
+        rounded=True,
+        filled=True
+    )
