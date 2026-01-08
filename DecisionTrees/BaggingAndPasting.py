f19f91e9155b1cd3fa2cf59453f17ae663ff7e12
Simon-Cheek <simon.cheek@yahoo.com>
2026-01-07 19:01:01 -0500

    Random Forests

--- DecisionTrees/BaggingAndPasting.py
+++ DecisionTrees/BaggingAndPasting.py
@@ -1,7 +1,7 @@
 from sklearn.datasets import load_iris
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import BaggingClassifier
+from sklearn.ensemble import BaggingClassifier, RandomForestClassifier
 
 iris = load_iris(as_frame=True)
 X_iris = iris.data[["petal length (cm)", "petal width (cm)"]].values
@@ -14,7 +14,14 @@ dt_clf.fit(X_train, y_train)
 print(dt_clf.score(X_test, y_test))
 
 # Now with Bagging
-bag_clf = BaggingClassifier(DecisionTreeClassifier(max_depth=2), n_estimators=100, n_jobs=-1)
+bag_clf = BaggingClassifier(DecisionTreeClassifier(max_depth=2), n_estimators=100, n_jobs=-1, oob_score=True)
 bag_clf.fit(X_train, y_train)
 print(bag_clf.score(X_test, y_test))
+print(bag_clf.oob_score_)
 
+# Now using Random Forest Classifier
+rndf_clf = RandomForestClassifier(n_estimators=100, n_jobs=-1, max_leaf_nodes=16)
+rndf_clf.fit(X_train, y_train)
+print(rndf_clf.score(X_test, y_test))
+for score, name in zip(rndf_clf.feature_importances_, ["Petal Length", "Petal Width"]):
+     print(round(score, 2), name)
